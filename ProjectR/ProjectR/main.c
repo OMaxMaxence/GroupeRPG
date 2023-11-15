59fe6cbe442df0414210683925d3607ccd84badc
MaxenceM <150232355+OMaxMaxence@users.noreply.github.com>
2023-11-15 11:51:36 +0100

    map

--- ProjectR/ProjectR/main.c
+++ ProjectR/ProjectR/main.c
@@ -12,11 +12,12 @@ int main()
 	window = sfRenderWindow_create(mode, titre, sfDefaultStyle, NULL);
 	sfEvent event;
 
-	
-	
+	float timer = 0;
+	float timerAnim = 0;
+	initTools();
 	while (sfRenderWindow_isOpen(window))
 	{
-
+		restartClock();
 		while (sfRenderWindow_pollEvent(window, &event))
 		{
 			if (event.type == sfEvtClosed)
@@ -25,9 +26,17 @@ int main()
 			}
 		}
 		initMenu();
+		initMap();
 		
+
+
+		/*timer += 0.1f * getDeltaTime();
+		if (timer >= 4.0f) timer = 0;
+		timerAnim += 0.001f * getDeltaTime();
+		if (timerAnim >= 4.0f) timerAnim = 0;*/
 		sfRenderWindow_clear(window, sfBlack);
 		displayMenu(window);
+		displayMap(window, timerAnim);
 		sfRenderWindow_display(window);
 	}
 }
---
+++ ProjectR/ProjectR/map.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "tools.h"
+
+
+
+void initMap();
+void updateMap(sfRenderWindow* _window, float _t, sfView* _view);
+void displayMap(sfRenderWindow* _window, float _t);
